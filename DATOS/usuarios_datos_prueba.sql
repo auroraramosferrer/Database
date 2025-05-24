28d69953472844804a73af3f5d54d104d0f3c416
Aurora Ramos Ferrer <1173752@alu.murciaeduca.es>
2025-05-24 19:43:55 +0200

    BD

---
+++ DATOS/usuarios_datos_prueba.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.usuarios (nombre,contrasena) VALUES
+	 ('juan','$2b$10$g7kR2WN6iH7.lVtmGvLhuOFLsZDSpOI7QRW6wjb3MpUJnzIqJ1bvy'),
+	 ('usuario','$2b$10$r.jZ3gVThI2bYakiUTW8KeKjuRYDBTI/efJla.mrEUPTX5yVxS4gy'),
+	 ('paco','$2b$10$0YEEWVYDJBI850pmLXENYOfH04MXpZ9FXRt6P2F/grJ4ZroM8mTP6');
